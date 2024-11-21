5067fe53d8e1364f9a61de7e282381d13f76e5bd
pabello-2137 <harztagowypl@gmail.com>
2024-11-21 13:02:06 +0100

    Add files via upload

---
+++ rozkład-liczb.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int k;
+    cout << "podaj liczbe KURWA: ";
+    cin >> n;
+    k = 2;
+    while (n > 1) {
+        while (n % k == 0) {
+            cout << k << endl;
+            n = n / k;
+            k++;
+       }
+    }
+}
